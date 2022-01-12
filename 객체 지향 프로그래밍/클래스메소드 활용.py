238ac7faeaa9b184f52b0181b1755d4945d864ee
oocheol <oocheol@naver.com>
2022-01-13 01:33:37 +0900

    22/01/13

---
+++ 객체 지향 프로그래밍/클래스메소드 활용.py
@@ -0,0 +1,29 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def from_string(cls, string_params):
+        string = string_params.split(",")
+        name = string[0]
+        email = string[1]
+        password = string[2]
+        
+        cls(name, email, password)
+        
+    @classmethod
+    def from_list(cls, list_params):
+        name = list_params[0]
+        email = list_params[1]
+        password = list_params[2]
+        
+        cls(name, email, password)
+
+# 유저 생성 및 초기값 설정
+younghoon = User.from_string("강영훈,younghoon@codeit.kr,123456")
+yoonsoo = User.from_list(["이윤수", "yoonsoo@codeit.kr", "abcdef"])
+
+print(younghoon.name, younghoon.email, younghoon.password)
+print(yoonsoo.name, yoonsoo.email, yoonsoo.password)
